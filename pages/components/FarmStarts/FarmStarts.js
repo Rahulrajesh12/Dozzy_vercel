6ebf69ab1c3895db2de4b90aae9f6806b961d1e5
sonusatish34 <sonusatish34@gmail.com>
2024-09-26 18:36:50 +0530

    dozzy starts at price changed

--- pages/components/FarmStarts/FarmStarts.js
+++ pages/components/FarmStarts/FarmStarts.js
@@ -7,7 +7,7 @@ import Link from 'next/link';
 const FarmStarts = () => {
     return (
         <div className='py-6 px-4 md:px-20'>
-            <p className='pb-4 font-semibold text-xl lg:text-3xl pt-4 lg:pt-14 md:pb-7 font-opensans'>Farmhouses Starts At ₹5999/Day</p>
+            <p className='pb-4 font-semibold text-xl lg:text-3xl pt-4 lg:pt-14 md:pb-7 font-opensans'>Farmhouses Starts At ₹7999/Day</p>
             <div className='flex'>
                 <div >
                     <Image
@@ -20,7 +20,7 @@ const FarmStarts = () => {
                     />
                 </div>
                 <div className='w-3/5 lg:pl-10'>
-                    <p className='text-[10px] font-normal pl-3  md:text-xl lg:text-2xl lg:w-[70%] mont-text'>Dozzy Has 10,000+ Attached Farmhouses From Basic - Luxury It's Just Starting At  ₹5999/Day </p>
+                    <p className='text-[10px] font-normal pl-3  md:text-xl lg:text-2xl lg:w-[70%] mont-text'>Dozzy Has 10,000+ Attached Farmhouses From Basic - Luxury It's Just Starting At  ₹7999/Day </p>
                     <p className='text-[11px] font-semibold pl-3  md:text-xl lg:text-3xl pt-1 lg:pt-8'>To View More</p>
                     <div className='flex gap-1 lg:gap-3 text-white pt-2  md:pt-6 pl-1'>
                         <button className='bg-green-500 rounded-md py-1 px-1 md:px-4 md:py-2'>
