45001ceea68dcf800edd4f3c861084029f869dab
RahulRajesh <rahulrajesheerla@gmail.com>
2024-11-12 15:01:45 +0530

    added loaded

--- pages/components/Header/Header.js
+++ pages/components/Header/Header.js
@@ -1,15 +1,14 @@
 
-
-import React from 'react'
+import React from 'react';
 import Link from 'next/link';
 import { FiPhoneCall } from "react-icons/fi";
 import LinkCall from '../LinkCall';
 import { useRouter } from 'next/router';
 
 const Header = () => {
-const router = useRouter();
+  const router = useRouter();
 
- const handleLogoClick = (e) => {
+  const handleLogoClick = (e) => {
     if (router.pathname === '/') {
       e.preventDefault();
       router.reload();
@@ -18,16 +17,19 @@ const router = useRouter();
 
   return (
     <div className='font-bold text-lg px-5 md:px-12 lg:px-20 py-4 flex justify-between items-center'>
-      <Link href={'/'}><h2 className='text-3xl lg:text-5xl text-[#556EE6]'>Dozzy</h2></Link>
+      <Link href={'/'} onClick={handleLogoClick}>
+        <h2 className='text-3xl lg:text-5xl text-[#556EE6]'>Dozzy</h2>
+      </Link>
       <div>
-        <div className=' py-2 text-black'>
-          <p className='lg:pb- lg:text-3xl text-sm  text-[#556EE6]'>For Booking help</p>
-          <Link onClick={(e) => LinkCall(e,"tel:9666655973")} className='lg:text-3xl lg:pt-2 pt-1 font-bold text-sm flex items-center gap-1 lg:gap-2' href="tel:9666655973" target='_blank'><span><FiPhoneCall className='size-3 lg:size-6' /></span>96666-559-73<span></span></Link>
-          
+        <div className='py-2 text-black'>
+          <p className='lg:pb- lg:text-3xl text-sm text-[#556EE6]'>For Booking help</p>
+          <Link onClick={(e) => LinkCall(e, "tel:9666655973")} className='lg:text-3xl lg:pt-2 pt-1 font-bold text-sm flex items-center gap-1 lg:gap-2' href="tel:9666655973" target='_blank'>
+            <span><FiPhoneCall className='size-3 lg:size-6' /></span>96666-559-73
+          </Link>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
